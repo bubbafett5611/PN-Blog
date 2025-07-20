34df1d3dcacc1738f99b8620da2b98ac05df8a0e
bubbafett5611 <metalgfx@gmail.com>
2025-07-20 02:03:51 +0100

    config fix

--- blog/.vitepress/config.mjs
+++ blog/.vitepress/config.mjs
@@ -8,7 +8,7 @@ export default defineConfig({
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: 'Home', link: '/PN-Blog/' },
+      { text: 'Home', link: '/' },
     ],
     search: {
       provider: 'local'
