ffc293f2254e2591baaaddb5a5dbce91d6196d29
bubbafett5611 <metalgfx@gmail.com>
2025-07-20 02:15:18 +0100

    one more time with feeling

--- blog/.vitepress/theme/components/CardContainer.vue
+++ blog/.vitepress/theme/components/CardContainer.vue
@@ -1,6 +1,7 @@
 <script setup>
 import ArticleCard from "./ArticleCard.vue";
 import { data as articles } from './../index.data.js'
+import { withBase } from 'vitepress'
 </script>
 <template>
     <div class="container mx-auto flex flex-wrap justify-center gap-x-4 gap-y-4">
