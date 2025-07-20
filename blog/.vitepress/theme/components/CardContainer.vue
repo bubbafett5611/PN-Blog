6406c9432be42c2154ce24740f07ca7cca294c95
bubbafett5611 <metalgfx@gmail.com>
2025-07-20 02:12:50 +0100

    base fix

--- blog/.vitepress/theme/components/CardContainer.vue
+++ blog/.vitepress/theme/components/CardContainer.vue
@@ -4,6 +4,6 @@ import { data as articles } from './../index.data.js'
 </script>
 <template>
     <div class="container mx-auto flex flex-wrap justify-center gap-x-4 gap-y-4">
-        <ArticleCard v-for="article in articles" :title="article.title" :href="article.url" :date="article.date.string" :image="article.image" :excerpt="article.excerpt"/>
+        <ArticleCard v-for="article in articles" :title="article.title" :href="withBase(article.url)" :date="article.date.string" :image="article.image" :excerpt="article.excerpt"/>
     </div>
 </template>
